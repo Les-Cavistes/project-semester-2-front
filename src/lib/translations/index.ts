c1d2fe34332795613f97334e1e96a89be6cb9221
Tom Planche <tomplanche@proton.me>
2025-05-07 14:11:27 +0200

    [14] (feat on i18n) i18n setup and initialization.

---
+++ src/lib/translations/index.ts
@@ -0,0 +1,29 @@
+import i18n, { type Config } from "sveltekit-i18n";
+import lang from "./lang.json";
+
+export const config: Config = {
+  translations: {
+    en: { lang },
+    fr: { lang },
+  },
+  loaders: [
+    {
+      locale: "en",
+      key: "main",
+      loader: async () => (await import("./en/content.json")).default,
+    },
+    {
+      locale: "fr",
+      key: "main",
+      loader: async () => (await import("./fr/content.json")).default,
+    },
+  ],
+};
+
+export const { t, loading, locales, locale, loadTranslations } = new i18n(
+  config,
+);
+
+loading.subscribe(
+  ($loading) => $loading && console.log("Loading translations..."),
+);
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+// Imports
+import { locale, locales, t } from "$lib/translations";
+
+// Variables
+
+// Functions
+</script>
+
+<select bind:value="{$locale}">
+  {#each $locales as value}
+    <option value="{value}">{$t(`lang.${value}`)}</option>
+  {/each}
+</select>
+
+<main>
+  <slot/>
+</main>
